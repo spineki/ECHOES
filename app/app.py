b08e9a061a1229b9db08f526d987ac796f0fb1f0
Marras Antoine <antoine.marras@student-cs.fr>
2020-10-17 01:33:03 +0200

    authors done

--- app/app.py
+++ app/app.py
@@ -1,23 +1,54 @@
+import os
 from flask import Flask, render_template, send_from_directory
+from flask.helpers import send_file
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
-
+app.config.from_pyfile('./config.cfg')
 
 @app.route('/')
 def index():
     return "Hello world !"
 
+@app.route('/authors/')
+def authors():
+    #onlyfiles = [f for f in listdir(app.config['BOOK_LOCATION']) if isfile(join(app.config['BOOK_LOCATION'], f))]
 
-@app.route("/book/<book_name>/")
-def book(book_name):
+    folders = os.listdir(app.config['BOOK_LOCATION'])
+
+    return render_template("author.html",authors = folders)
+
+@app.route('/authors/<author_name>/')
+def author(author_name):
+    #onlyfiles = [f for f in listdir(app.config['BOOK_LOCATION']) if isfile(join(app.config['BOOK_LOCATION'], f))]
+
+    author_folder_dir = os.path.join(app.config['BOOK_LOCATION'], author_name )
+
+    books_folder = os.listdir( author_folder_dir)
+
+
+    files = []
+    for book_folder in books_folder:
+
+        book_dir = os.path.join(author_folder_dir, book_folder)
+        book_folder_content = os.listdir(book_dir)
+
+        for file in book_folder_content:
+            if len(file) > 4 and file[-5:] == ".epub":
+                files.append({"book_name": file, "book_folder": book_folder})
+
+    return render_template("author_book.html", author=author_name,  books=files)
+
+
+@app.route('/authors/<author_name>/<book_folder>/<book_name>')
+def book(author_name, book_folder, book_name):
     try:
-        return send_from_directory(directory="./static/data/", filename=book_name,  as_attachment=True)
+        return send_from_directory(directory= os.path.join(app.config['BOOK_LOCATION'], author_name, book_folder) , filename=book_name,  as_attachment=True)
     except Exception as e:
         return "error: " + str(e)
 
     
-
-
 
 @app.route("/about/")
 def about():
