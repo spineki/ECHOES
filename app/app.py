9451548086ade775020674780ef941eca04d2dba
Marras Antoine <antoine.marras@student-cs.fr>
2020-10-24 02:17:56 +0200

    added search with database by book name

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, send_from_directory, request, url_for
+from flask import Flask, render_template, send_from_directory, request, url_for, redirect
 from flask.helpers import send_file
 from urllib.parse import unquote, quote
 import sqlite3
@@ -26,50 +26,128 @@ NUMBER_ITEM_PER_PAGE = 5
 def override_url_for():
     return dict(url_for=dated_url_for)
 
+
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
         filename = values.get('filename', None)
         if filename:
             file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
+                                     endpoint, filename)
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
+
 def get_author_folders():
     return os.listdir(app.config['BOOK_LOCATION'])
+
+
+def fetch_books_by_name(name):
+
+    # SELECT title, author_sort FROM books
+    # SELECT sort from authors
+    # PRAGMA table_info(authors)
+
+    conn = sqlite3.connect(os.path.join(
+        app.config['BOOK_LOCATION'], "metadata.db"))
+
+    files = []
+
+    results = conn.execute(
+        """SELECT path from books where title LIKE "%{0}%"; """.format(name))
+    for row in results:
+        path = row[0]
+        path_data = path.split("/")
+        author_folder = path_data[0]
+        book_folder = path_data[1]
+
+        author_folder_dir = os.path.join(
+            app.config['BOOK_LOCATION'], author_folder)
+
+        book_dir = os.path.join(author_folder_dir, book_folder)
+        book_folder_content = os.listdir(book_dir)
+
+        for file in book_folder_content:
+            if len(file) > 4 and file[-5:] == ".epub":
+                files.append({"book_name": quote(file),
+                              "book_folder": quote(book_folder),
+                              "book_author": quote(author_folder)
+                              })
+
+    return files
+    # now we have the list of author name
+
 
 def get_author_names():
     return AUTHORS
 
 # HOME PAGE
+
+
 @app.route('/')
 def index():
     return render_template("home.html")
 
 # GETTING EVERY AUTHOR
-@app.route('/authors/')
-def authors():
-    #onlyfiles = [f for f in listdir(app.config['BOOK_LOCATION']) if isfile(join(app.config['BOOK_LOCATION'], f))]
+
+
+@app.route('/authors/page/<page>')
+def authors_pages(page):
+    page = int(page)
+
     author_names = get_author_names()
-    return render_template("author.html", authors=author_names, unquote=unquote)
+
+    filtered_author_names = author_names[page *
+                                         NUMBER_ITEM_PER_PAGE:(page+1) * NUMBER_ITEM_PER_PAGE]
+
+    max_page = len(author_names) // NUMBER_ITEM_PER_PAGE - 1
+
+    previous_page = max(page - 1, 0)
+    next_page = min(page + 1, max_page)
+
+    return render_template("author.html", authors=filtered_author_names, previous_page=previous_page, next_page=next_page, unquote=unquote)
+
 
 # GETTING AUTHOR FILTERED BY SEARCH
+
+@app.route('/reload/', methods=['POST'])
+def reload():
+    global AUTHORS
+    AUTHORS = get_author_folders()
+    return redirect('/authors/page/0', code=302)
+
+
 @app.route('/authors/search/', methods=['POST'])
 def search():
     data = request.form
-    search_keyword = data["search"].lower()
+    search_keyword = data["search"].lower().strip()
 
     author_names = get_author_names()
 
-    filtered_author_names = []
-    for author_name in author_names:
-        if search_keyword in author_name.lower():
-            filtered_author_names.append(author_name)
+    if len(search_keyword) == 0:
+        return render_template("author.html", authors=[], unquote=unquote)
 
-    return render_template("author.html", authors=filtered_author_names, unquote=unquote)
+    # we know that we have a non empty search word here
+    if search_keyword[0] == "@":
+        search_keyword_book = search_keyword[1:]
+        print("searching", search_keyword_book)
 
+        author_name = "Auteurs multiples?"
+
+        files = fetch_books_by_name(search_keyword_book)
+
+        return render_template("author_book.html", author=author_name,  books=files, unquote=unquote)
+
+    else:
+        filtered_author_names = []
+        for author_name in author_names:
+            if search_keyword in author_name.lower():
+                filtered_author_names.append(author_name)
+
+        return render_template("author.html", authors=filtered_author_names, unquote=unquote)
 
 # GETTING EVERY BOOK FROM ONE AUTHOR
+
+
 @app.route('/authors/<author_name>/')
 def author(author_name):
     #onlyfiles = [f for f in listdir(app.config['BOOK_LOCATION']) if isfile(join(app.config['BOOK_LOCATION'], f))]
@@ -87,11 +165,15 @@ def author(author_name):
         for file in book_folder_content:
             if len(file) > 4 and file[-5:] == ".epub":
                 files.append({"book_name": quote(file),
-                              "book_folder": quote(book_folder)})
+                              "book_folder": quote(book_folder),
+                              "book_author": quote(author_name)
+                              })
 
     return render_template("author_book.html", author=author_name,  books=files, unquote=unquote)
 
 # GETTING A BOOK
+
+
 @app.route('/authors/<author_name>/<book_folder>/<book_name>')
 def book(author_name, book_folder, book_name):
     author_name = unquote(author_name)
@@ -106,8 +188,10 @@ def book(author_name, book_folder, book_name):
 
 if __name__ == "__main__":
 
+    # fetch_books_by_name("an")
+
     # INIT CACE
     AUTHORS = get_author_folders()
-
+    #AUTHORS = [str(i) for i in range(30)]
 
     app.run(debug=True, host=app.config["IP_ADDRESS"])
