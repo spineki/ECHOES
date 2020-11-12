1841be90a572228b05712602432287ff8abdcdca
Marras Antoine <antoine.marras@student-cs.fr>
2020-11-12 09:27:13 +0100

    code devided in two sections

--- app/fourtoutici.py
+++ app/fourtoutici.py
@@ -1,75 +1,125 @@
+from typing import Dict, List, Tuple
 import requests
 import bs4
 import json
-r = requests.get(
-    "http://www.fourtoutici.top/search.php?action=showsearchresults&q=+BrandoN+mull&listyear=20xx&search=Recherche")
-
-soup = bs4.BeautifulSoup(r.content, features="html.parser")
-
-print(r.status_code)
-
-results = soup.select("a")
 
 
-filtered_results = []
-for result in results:
+def get_download_page_links(keyword: str) -> Tuple[List[Dict[str, str]], int]:
+    """
+    Une fonction cherchant le mot clef en en ligne et renvoyant la liste des liens vers
+    les pages de téléchargement pour chaque résultat.
+
+
+    Argument:
+        keyword: str:
+    Return:
+        (list de lien, code de résultat https)
+
+    """
+
+    keyword_plus = "+".join(keyword.split(" "))
+
+    r = requests.get(
+        "http://www.fourtoutici.top/search.php?action=showsearchresults&q=+{0}&listyear=20xx&search=Recherche".format(keyword_plus))
+
+    soup = bs4.BeautifulSoup(r.content, features="html.parser")
+
+    rows = soup.select("table > tbody td > img")
+
+    if r.status_code == 200:
+        results = soup.select("a")
+
+        filtered_results = []
+        for result in results:
+            try:
+                content = ""
+                if result['href'][:len("javascript")] == "javascript":
+
+                    url = result['href']
+
+                    content = "[" + url[len("javascript:popupup")+1:-1] + "]"
+
+                    # Problème, les ' dans les mots ne sont pas décodable par json.
+                    # on les transforme en ""
+                    content = content.replace("'", "\"")
+
+                    json_content = json.loads(content)
+
+                    row_dict = {
+                        "title": json_content[0],
+                        "directory": json_content[1]
+                    }
+
+                    filtered_results.append(row_dict)
+            except Exception as e:
+                print(e, content)
+
+        return (filtered_results, 200)
+
+    else:
+        print("Impossible d'accéder au serveur")
+        return ([], r.status_code)
+
+
+def download_file(title, directory):
+    """
+    Download a file from the website solving the little number puzzle
+    """
+
+    download_link = "http://www.fourtoutici.top/upload.php?action=downloadfile&filename={0}&directory={1}&".format(
+        title, directory)
+
+    r = requests.get(download_link)
+
+    if r.status_code != 200:
+        print("impossible to reach")
+        return r.status_code
+
+    soup = bs4.BeautifulSoup(r.content, features="html.parser")
+
     try:
-        url = ""
-        content = ""
-        if result['href'][:len("javascript")] == "javascript":
+        raw_code = None
+        images = soup.select("img")
+    except:
+        print("impossible to get img tag in download page")
+        return -1
 
-            url = result['href']
+    for image in images:
+        try:
+            if image["alt"] == "Code de sécurité à recopier":
+                raw_code = image["src"].split("?")[-1]
+                break
+        except:
+            pass
 
-            content = "[" + url[len("javascript:popupup")+1:-1] + "]"
-            content = content.replace("'", "\"")
+    # todo, reanalyse first
+    first = "6"
+    center = raw_code[-2:]
+    last = raw_code[-3]
+    code = first + center + last
 
-            filtered_results.append(json.loads(content))
-    except Exception as e:
-        print(e, content)
+    print("attempting to beat code ", raw_code, " with ", code)
+
+    download_link = "http://www.fourtoutici.top/upload.php?action=download&directory={1}&filename={0}&".format(
+        title.replace(" ", "+"), directory)
+
+    file_download_link = download_link + "valcodeup=" + code
+
+    r = requests.get(file_download_link)
+
+    if r.status_code != 200:
+        print("impossible to reach file ", r.status_code)
+        return r.status_code
+
+    with open(title, "wb+") as f:
+        f.write(r.content)
+
+
+filtered_results, error_code = get_download_page_links("les sables")
 
 chosen_result = filtered_results[0]
 
-
-download_link = "http://www.fourtoutici.top/upload.php?action=downloadfile&filename={0}&directory={1}&".format(
-    chosen_result[0], chosen_result[1])
-"http://www.fourtoutici.top/upload.php?action=download&directory={}&filename={}&valcodeup=6"
-r = requests.get(download_link)
-
-soup = bs4.BeautifulSoup(r.content, features="html.parser")
-
-raw_code = None
-images = soup.select("img")
-for image in images:
-    try:
-        if image["alt"] == "Code de sécurité à recopier":
-            raw_code = image["src"].split("?")[-1]
-            break
-    except:
-        pass
-
-print(raw_code)
-
-# todo, reanalyse first
-first = "6"
-center = raw_code[-2:]
-last = raw_code[-3]
-code = first + center + last
-
-print(code)
-
-print(download_link)
-
-download_link = "http://www.fourtoutici.top/upload.php?action=download&directory={1}&filename={0}&".format(
-    chosen_result[0].replace(" ", "+"), chosen_result[1])
-
-
-file_download_link = download_link + "valcodeup=" + code
-print(file_download_link)
-r = requests.get(file_download_link)
-print(r.status_code)
-
-with open("export.epub", "wb+") as f:
-    f.write(r.content)
+download_file(chosen_result['title'], chosen_result['directory'])
 
 
 """
